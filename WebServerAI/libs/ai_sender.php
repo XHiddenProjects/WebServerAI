fac05e1e512bb58e966f1197b098c8e5e5c9a718
XHiddenProjects <64079569+XHiddenProjects@users.noreply.github.com>
2024-05-31 19:54:39 -0400

    Delete WebServerAI directory

--- WebServerAI/libs/ai_sender.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace WebServerAI;
-use WebServerAI\Trainer;
-require_once(dirname(__DIR__,2).'/WebServerAI/libs/trainer.php');
-
-$insertInfo = new Trainer();
-echo $insertInfo->format($insertInfo->decodeURI($_REQUEST['wsaai']));
-?>
